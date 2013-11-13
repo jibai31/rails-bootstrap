968e35caf24114524673693b7a50361619ae8251
Jean-Baptiste Goulain <jibai31@gmail.com>
2013-11-13 19:53:12 +0000

    Configure Devise and OmniAuth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauth_providers => [:google_oauth2, :facebook, :twitter]
 
   has_many :authentications
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -228,6 +228,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
+    { access_type: "offline", approval_prompt: "" }
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+    { scope: 'publish_stream, email', strategy_class: OmniAuth::Strategies::Facebook }
+  config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
